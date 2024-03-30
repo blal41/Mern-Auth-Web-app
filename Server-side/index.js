abbc27cf729b1fa5b56b20ef626f52352a56edc4
Babulal Seervi <babuseervi4130@gmail.com>
2024-03-30 16:58:14 +0530

    Update the dir name

--- Server-side/index.js
+++ Server-side/index.js
@@ -25,10 +25,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend-app/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend-app", "dist", "index.html"));
 });
 
 
